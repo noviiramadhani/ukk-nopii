4a597200deb65f7afb97c00ba0832e16c06af201
noviiramadhani <132984932+noviiramadhani@users.noreply.github.com>
2024-03-06 19:48:44 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Galeri Foto Novi</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+<style>
+        body{
+            background-image: url('assets/img/diagonal.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            font-family: Times, sans-serif; 
+            color: black; 
+        }
+
+        .display-4 {
+            font-family: 'Times New Roman', Times, serif; 
+        }
+       
+    </style>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Website Galeri Foto Novii</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+        
+      </div>
+      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8 text-center">
+            <h1 class="display-4 mb-3">Welcome To The Novii Photo Gallery Website!</h>
+            <p class="lead">Discover and share your beautiful moments with this photo gallery</p>
+        </div>
+    </div>
+</div>
+
+<footer class="d-flex justify-content-center botder-top mt-3 bg-light fixed-bottom">
+    <p>&copy; UKK RPL 2024 |  Novi Ramadhani</p>
+</footer>
+
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
